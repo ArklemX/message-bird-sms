9594ec76bebdc0b8ebbb8be1cc4e5b93692fbe8d
ArklemX <avaikamarcel@gmail.com>
2022-12-12 22:14:34 +0100

    class updated

--- src/MessageBird.php
+++ src/MessageBird.php
@@ -7,23 +7,23 @@ use MessageBird\Objects\Message;
 class MessageBird
 {
 
-    protected $accessKey;
-    protected $originator;
-    protected $receiver;
-    protected $message;
+//    protected $accessKey;
+//    protected $originator;
+//    protected $receiver;
+//    protected $message;
 
-    /**
-     * @param $accessKey
-     * @param $originator
-     */
-    public function __construct($accessKey, $originator)
-    {
-        //Set the accesskey with which we'll send our messages
-        $this->setAccessKey($accessKey);
-
-        //Set the Originator : Who the message come from
-        $this->setOriginator($originator);
-    }
+//    /**
+//     * @param $accessKey
+//     * @param $originator
+//     */
+//    public function __construct($accessKey, $originator)
+//    {
+//        //Set the accesskey with which we'll send our messages
+//        $this->setAccessKey($accessKey);
+//
+//        //Set the Originator : Who the message come from
+//        $this->setOriginator($originator);
+//    }
 
     /**
      * @param array $receivers
@@ -34,80 +34,81 @@ class MessageBird
      * @throws Exceptions\HttpException
      * @throws \JsonException
      */
-    public function send(array $receivers, $message)
+    public static function send($accesskey, $originator,array $receivers, $message)
     {
-        $MessageBird = new Client($this->getAccessKey());
+        $MessageBird = new Client($accesskey);
         $Message = new Message();
-        $Message->originator = $this->getOriginator();
+        $Message->originator = $originator;
         $Message->recipients = array($receivers);
         $Message->body = $message;
 
         $MessageBird->messages->create($Message);
     }
 
-    /**
-     * @return mixed
-     */
-    public function getAccessKey()
-    {
-        return $this->accessKey;
-    }
-
-    /**
-     * @param mixed $accessKey
-     */
-    public function setAccessKey($accessKey)
-    {
-        $this->accessKey = $accessKey;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getOriginator()
-    {
-        return $this->originator;
-    }
-
-    /**
-     * @param mixed $originator
-     */
-    public function setOriginator($originator)
-    {
-        $this->originator = $originator;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getReceiver()
-    {
-        return $this->receiver;
-    }
-
-    /**
-     * @param mixed $receiver
-     */
-    public function setReceiver($receiver)
-    {
-        $this->receiver = $receiver;
-    }
-
-    /**
-     * @return mixed
-     */
-    public function getMessage()
-    {
-        return $this->message;
-    }
-
-    /**
-     * @param mixed $message
-     */
-    public function setMessage($message)
-    {
-        $this->message = $message;
-    }
-
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getAccessKey()
+//    {
+//        return $this->accessKey;
+//    }
+//
+//    /**
+//     * @param mixed $accessKey
+//     */
+//    public function setAccessKey($accessKey)
+//    {
+//        $this->accessKey = $accessKey;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getOriginator()
+//    {
+//        return $this->originator;
+//    }
+//
+//    /**
+//     * @param mixed $originator
+//     */
+//    public function setOriginator($originator)
+//    {
+//        $this->originator = $originator;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getReceiver()
+//    {
+//        return $this->receiver;
+//    }
+//
+//    /**
+//     * @param mixed $receiver
+//     */
+//    public function setReceiver($receiver)
+//    {
+//        $this->receiver = $receiver;
+//    }
+//
+//    /**
+//     * @return mixed
+//     */
+//    public function getMessage()
+//    {
+//        return $this->message;
+//    }
+//
+//    /**
+//     * @param mixed $message
+//     */
+//    public function setMessage($message)
+//    {
+//        $this->message = $message;
+//    }
+//
 
 }
